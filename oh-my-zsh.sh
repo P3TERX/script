a9fd96f7c486ec6c7be0f43e8788ad554324d5db
P3TERX <p3terx@outlook.com>
2020-05-07 03:30:13 +0800

    oh-my-zsh.sh: Error exit mechanism

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -4,7 +4,7 @@
 # File name: oh-my-zsh.sh
 # Description: Install a simple Oh-My-Zsh environment using Antigen
 # System Required: GNU/Linux
-# Version: 1.0
+# Version: 1.1
 # Lisence: MIT
 # Author: P3TERX
 # Blog: https://p3terx.com
@@ -16,11 +16,22 @@ Red_background_prefix="\033[41;37m"
 Font_color_suffix="\033[0m"
 INFO="[${Green_font_prefix}INFO${Font_color_suffix}]"
 ERROR="[${Red_font_prefix}ERROR${Font_color_suffix}]"
-[ $(command -v zsh) ] || echo -e "${ERROR} Zsh is not installed."
+
+[ $(command -v git) ] || {
+    echo -e "${ERROR} Git is not installed."
+    exit 1
+}
+
+[ $(command -v zsh) ] || {
+    echo -e "${ERROR} Zsh is not installed."
+    exit 1
+}
+
 echo -e "${INFO} Installation Antigen ..."
 rm -rf $HOME/.antigen
 mkdir -p $HOME/.antigen
-curl -L git.io/antigen > $HOME/.antigen/antigen.zsh
-curl -fsSL https://raw.githubusercontent.com/P3TERX/dotfiles/master/.zshrc > $HOME/.zshrc
+curl -L git.io/antigen >$HOME/.antigen/antigen.zsh
+curl -fsSL https://raw.githubusercontent.com/P3TERX/dotfiles/master/.zshrc >$HOME/.zshrc
+
 echo -e "${INFO} Installation Oh My Zsh ..."
 zsh $HOME/.zshrc
