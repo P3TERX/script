be0a51635fe3ea120c72a2e61929abf96e11cdc9
P3TERX <p3terx@outlook.com>
2020-07-21 07:21:16 +0800

    Add fclone.sh

---
+++ fclone.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/script
+# File name：fclone.sh
+# Description: Install the latest version fclone
+# Version: 1.0
+#
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+if [[ $(id -u) -ne 0 ]]; then
+    echo "This script must be run as root" 
+    exit 1
+fi
+
+TMPFILE=/tmp/fclone.zip
+CLDBIN=/usr/bin/fclone
+OSARCH=$(uname -m)
+case $OSARCH in 
+    x86_64)
+        BINTAG=linux-amd64
+        ;;
+    i*86)
+        BINTAG=linux-386
+        ;;
+    arm64)
+        BINTAG=linux-arm64
+        ;;
+    arm*)
+        BINTAG=linux-arm
+        ;;
+    *)
+        echo "unsupported OSARCH: $OSARCH"
+        exit 1
+        ;;
+esac
+
+wget -qO- https://api.github.com/repos/mawaya/rclone/releases/latest \
+| grep browser_download_url | grep "$BINTAG" | cut -d '"' -f 4 \
+| wget -nv -O "${TMPFILE}" -i-
+
+unzip -jo "${TMPFILE}" -d "${CLDBIN%/*}"
+rm -vf "${TMPFILE}"
+chmod +x "${CLDBIN}"
+
+fclone version
