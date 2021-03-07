2ac02ed294ad47cea4f535005d831361ca849db6
P3TERX <25927179+P3TERX@users.noreply.github.com>
2021-03-07 15:59:31 +0800

    lsd.sh: v1.0

--- lsd.sh
+++ lsd.sh
@@ -9,7 +9,7 @@
 # File name: lsd.sh
 # Description: Install latest version LSD (LSDeluxe)
 # System Required: GNU/Linux
-# Version: 0.9
+# Version: 1.0
 #
 
 set -o errexit
@@ -26,8 +26,7 @@ INFO="[${Green_font_prefix}INFO${Font_color_suffix}]"
 ERROR="[${Red_font_prefix}ERROR${Font_color_suffix}]"
 
 PROJECT_NAME='LSD (LSDeluxe)'
-#GH_API_URL='https://api.github.com/repos/Peltoche/lsd/releases/latest'
-GH_API_URL='https://api.github.com/repos/P3TERX/lsd/releases/latest'
+GH_API_URL='https://api.github.com/repos/Peltoche/lsd/releases/latest'
 BIN_NAME='lsd'
 BIN_DIR='/usr/local/bin'
 BIN_FILE="${BIN_DIR}/${BIN_NAME}"
